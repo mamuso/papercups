949277873f241265431b3dcc10664259eaede77e
Manuel Munoz Solera <mamuso@doh.gateway.sonic.net>
2018-06-16 23:31:13 -0700

    Getting the image URL

--- _tasks/pourcups.js
+++ _tasks/pourcups.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const fs = require("fs");
 const dateFormat = require("dateformat");
 const slugify = require("slugify");
+const ogs = require("open-graph-scraper");
 
 const Firestore = require("@google-cloud/firestore");
 const firestore = new Firestore({
@@ -16,45 +17,61 @@ const firestore = new Firestore({
 const document = firestore.collection("papercups");
 
 let query = firestore.collection("papercups");
-query
-  .orderBy("created_at", "desc")
-  .get()
-  .then(cups => {
-    cups.docs.forEach(sip => {
-      const date = dateFormat(sip.get("created_at"), "yyyy-mm-dd");
-      const slug = slugify(sip.get("name"), {
-        lower: true,
-        remove: /[$*_+~.()'"!\-:@]/g
-      });
-      fs.writeFile(
-        `_posts/${date}-${slug}.md`,
-        setFrontMatter(
-          sip.get("name"),
-          sip.get("address"),
-          sip.get("city"),
-          sip.get("country"),
-          sip.get("instagram_id"),
-          sip.get("location")._latitude,
-          sip.get("location")._longitude
-        ),
-        function(err) {
-          if (err) {
-            return console.log(err);
-          }
-        }
-      );
-    });
-  });
+try {
+  query
+    .orderBy("created_at", "desc")
+    .get()
+    .then(cups => {
+      cups.docs.forEach(sip => {
+        const ogsoptions = {
+          url: `https://www.instagram.com/p/${sip.get("instagram_id")}/`
+        };
 
-function setFrontMatter(
+        ogs(ogsoptions)
+          .then(result => {
+            return result.data.ogImage.url;
+          })
+          .then(instagram_url => {
+            const date = dateFormat(sip.get("created_at"), "yyyy-mm-dd");
+            const slug = slugify(sip.get("name"), {
+              lower: true,
+              remove: /[$*_+~.()'"!\-:@]/g
+            });
+            fs.writeFile(
+              `_posts/${date}-${slug}.md`,
+              setFrontMatter(
+                sip.get("name"),
+                sip.get("address"),
+                sip.get("city"),
+                sip.get("country"),
+                sip.get("instagram_id"),
+                instagram_url,
+                sip.get("location")._latitude,
+                sip.get("location")._longitude
+              ),
+              err => {
+                if (err) {
+                  return console.log(err);
+                }
+              }
+            );
+          });
+      });
+    });
+} catch (err) {
+  console.log(err);
+}
+
+const setFrontMatter = (
   title,
   address,
   city,
   country,
   instagram_id,
+  instagram_image_url,
   latitude,
   longitude
-) {
+) => {
   return `---
 layout: default
 title: ${title}
@@ -62,8 +79,9 @@ address: ${address}
 city: ${city}
 country: ${country}
 instragram_id: ${instagram_id}
+instagram_image_url: ${instagram_image_url}
 location: 
   - ${latitude}
   - ${longitude}
 ---`;
-}
+};
