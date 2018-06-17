777702085e055d4863efd20ecb75c8dbd2659ee6
Manuel Munoz Solera <mamuso@doh.gateway.sonic.net>
2018-06-16 22:36:56 -0700

    Adding dateformat

---
+++ _tasks/pourcups.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const fs = require("fs");
+const dateFormat = require("dateformat");
+
+const Firestore = require("@google-cloud/firestore");
+const firestore = new Firestore({
+  credentials: {
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL
+  }
+});
+
+// May I have a latte or two?
+const document = firestore.collection("papercups");
+
+let query = firestore.collection("papercups");
+query
+  .orderBy("created_at", "desc")
+  .get()
+  .then(cups => {
+    cups.docs.forEach(sip => {
+      const date = dateFormat(sip.get("created_at"), "yyyy-mm-dd");
+      fs.writeFile(
+        `_posts/${date}-${sip.id}.md`,
+        setFrontMatter(sip.get("name")),
+        function(err) {
+          if (err) {
+            return console.log(err);
+          }
+        }
+      );
+    });
+  });
+
+function setFrontMatter(title) {
+  return `---
+layout: default
+title: ${title}
+---`;
+}
