1d2b7c5e7444fbe066ff4f98e2d694c3ee668fb9
Manuel Muñoz Solera <mamuso@mamuso.net>
2019-07-02 16:17:52 -0700

    stitched json and next.config

---
+++ next.config.js
@@ -0,0 +1,21 @@
+const data = require("./data.json");
+
+module.exports = {
+  async exportPathMap() {
+    const pages = data.reduce(
+      (pages, cup) =>
+        Object.assign({}, pages, {
+          [`/pour/${cup.slug}`]: {
+            page: "Post",
+            query: { slug: cup.slug }
+          }
+        }),
+      {}
+    );
+
+    // combine the map of post pages with the home
+    return Object.assign({}, pages, {
+      "/": { page: "/" }
+    });
+  }
+};
