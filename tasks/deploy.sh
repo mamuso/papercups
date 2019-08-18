3d6fe78e55e17ef5f19d4a67bc09220e5dabb278
Manuel Muñoz Solera <mamuso@mamuso.net>
2019-08-17 23:53:37 -0700

    Updating deploy script

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -12,7 +12,7 @@ fi
 touch out/.nojekyll
 echo 'papercups.mamuso.net' > out/CNAME
 
-gh-pages -d $PUBLIC_PATH -b gh-pages -r "https://${$GITHUB_TOKEN}@github.com/mamuso/papercups.git" - -u "github-actions-bot <support+actions@github.com>"
+gh-pages -d $PUBLIC_PATH -b gh-pages -r "https://${GITHUB_TOKEN}@github.com/mamuso/papercups.git" - -u "github-actions-bot <support+actions@github.com>"
 retval=$?
 if [ $retval -ne 0 ]; then
   echo "gh-pages failed: $retval"
